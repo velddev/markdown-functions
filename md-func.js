fe8c7468e1009a49e5ae0d5776e91dbae02d6660
Veld <velddev@gmail.com>
2018-02-26 19:34:19 +0100

    Fix: regex issue

--- md-func.js
+++ md-func.js
@@ -4,10 +4,8 @@ FunctionBinder = function(allFunctions) {
     _constructor();
 
     function _constructor() {
-        if(allFunctions != 'undefined')
-        {
-            for(var func in allFunctions)
-            {
+        if(allFunctions != 'undefined') {
+            for(var func in allFunctions) {
                 fnObject[allFunctions[func].name.toLowerCase()] = allFunctions[func];
             }
         }
@@ -15,21 +13,21 @@ FunctionBinder = function(allFunctions) {
 
     // TODO: write a small tokenizer to handle multiple args
     this.parseFunctions = function(markdown) {        
-        while(true)
-        {
-            matches = /\$([^ ])+\s([a-zA-Z\s]).+/g.exec(markdown);
+        while(true) {
+            matches = /\$([^ ])+(\s)?(([a-zA-Z\s])?.)+/g.exec(markdown);
 
             if(!matches) break;
     
             var x = matches[0];
-            var type = x.split(' ')[0].substring(1);
-            var tx = x.substring(x.split(' ')[0].length);
-            tx = tx.split('<')[0].trim();
+            
+            var type = x.split(' ')[0].substring(1).trim();
+            
+            var args = x.substring(x.split(' ')[0].length);
+        
             var output = "<strong>error: invalid function</strong>";
             
-            if(typeof fnObject[type] === "function")
-            {
-                output = fnObject[type](tx);
+            if(typeof fnObject[type] === "function") {
+                output = fnObject[type](args);
             }
             
             markdown = text.replace(x, output);
